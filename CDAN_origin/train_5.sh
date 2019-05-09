a50231bd3699bf6a2bc68a8b9e6397886f31a1dd
dingmyu <d130143597@163.com>
2019-05-09 19:08:47 +0800

    CDAN_origin

---
+++ CDAN_origin/train_5.sh
@@ -0,0 +1,2 @@
+now=`date +%Y-%m-%d,%H:%m:%s`
+srun -p AD  --gres=gpu:8 -n1 --job-name=cdan python -u train_image.py --gpu_id 0,1,2,3,4,5,6,7 --net ResNet50 --dset imagenet --t_dset_path /mnt/lustre/dingmingyu/Research/da_zsl/dataset/mini-imagenet/test_transfer_5.txt --s_dset_path /mnt/lustre/dingmingyu/Research/da_zsl/dataset/mini-imagenet/trainval_list.txt --output_dir output_5 2>&1|tee logs/mini-train5-${now}.log 
